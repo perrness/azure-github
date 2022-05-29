4b037940a496208ec2475cf89d584eda5e7241af
Per Næss <perness3009@gmail.com>
2022-05-29 11:29:09 +0200

    feat: remove client secret

--- modules/githubsecrets/githubcredentials.tf
+++ modules/githubsecrets/githubcredentials.tf
@@ -7,25 +7,26 @@ resource "azuread_application_federated_identity_credential" "main" {
   subject               = "repo:perrness/${var.repo_name}:ref:refs/heads/main"
 }
 
-resource "azuread_application_password" "main" {
-  display_name = "${var.repo_name}-client-secret"
-  application_object_id = var.object_id
-}
-
 data "github_actions_public_key" "main" {
   repository = var.repo_name
-}
-
-resource "github_actions_secret" "ad_client_id" {
-  repository       = var.repo_name
-  secret_name      = "AZURE_AD_CLIENT_ID"
-  plaintext_value  = var.application_id
 }
 
 resource "github_actions_secret" "ad_tenant_id" {
   repository       = var.repo_name
   secret_name      = "AZURE_AD_TENANT_ID"
   plaintext_value  = var.ad_tenant_id
+}
+
+resource "github_actions_secret" "subscription_id" {
+  repository       = var.repo_name
+  secret_name      = "AZURE_SUBSCRIPTION_ID"
+  plaintext_value  = var.subscription_id
+}
+
+resource "github_actions_secret" "ad_client_id" {
+  repository       = var.repo_name
+  secret_name      = "AZURE_AD_CLIENT_ID"
+  plaintext_value  = var.application_id
 }
 
 resource "github_actions_secret" "backend_container_name" {
@@ -50,16 +51,4 @@ resource "github_actions_secret" "backend_storage_account_name" {
   repository       = var.repo_name
   secret_name      = "AZURE_BACKEND_STORAGE_ACCOUNT_NAME"
   plaintext_value  = var.backend_storage_account_name
-}
-
-resource "github_actions_secret" "client_secret" {
-  repository       = var.repo_name
-  secret_name      = "AZURE_CLIENT_SECRET"
-  plaintext_value  = azuread_application_password.main.value
-}
-
-resource "github_actions_secret" "subscription_id" {
-  repository       = var.repo_name
-  secret_name      = "AZURE_SUBSCRIPTION_ID"
-  plaintext_value  = var.subscription_id
 }
