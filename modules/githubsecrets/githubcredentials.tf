d40d34e44db6f181d73077058fdaf63bbe916a0f
Per Næss <perness3009@gmail.com>
2022-07-15 21:12:20 +0200

    fix: set environment prod

--- modules/githubsecrets/githubcredentials.tf
+++ modules/githubsecrets/githubcredentials.tf
@@ -4,7 +4,7 @@ resource "azuread_application_federated_identity_credential" "main" {
   description           = "Deployments for ${var.repo_name}"
   audiences             = ["api://AzureADTokenExchange"]
   issuer                = "https://token.actions.githubusercontent.com"
-  subject               = "repo:perrness/${var.repo_name}:ref:refs/heads/main"
+  subject               = "repo:perrness/${var.repo_name}:environment:production"
 }
 
 data "github_actions_public_key" "main" {
@@ -12,43 +12,43 @@ data "github_actions_public_key" "main" {
 }
 
 resource "github_actions_secret" "ad_tenant_id" {
-  repository       = var.repo_name
-  secret_name      = "AZURE_AD_TENANT_ID"
-  plaintext_value  = var.ad_tenant_id
+  repository      = var.repo_name
+  secret_name     = "AZURE_AD_TENANT_ID"
+  plaintext_value = var.ad_tenant_id
 }
 
 resource "github_actions_secret" "subscription_id" {
-  repository       = var.repo_name
-  secret_name      = "AZURE_SUBSCRIPTION_ID"
-  plaintext_value  = var.subscription_id
+  repository      = var.repo_name
+  secret_name     = "AZURE_SUBSCRIPTION_ID"
+  plaintext_value = var.subscription_id
 }
 
 resource "github_actions_secret" "ad_client_id" {
-  repository       = var.repo_name
-  secret_name      = "AZURE_AD_CLIENT_ID"
-  plaintext_value  = var.application_id
+  repository      = var.repo_name
+  secret_name     = "AZURE_AD_CLIENT_ID"
+  plaintext_value = var.application_id
 }
 
 resource "github_actions_secret" "backend_container_name" {
-  repository       = var.repo_name
-  secret_name      = "AZURE_BACKEND_CONTAINER_NAME"
-  plaintext_value  = var.backend_container_name
+  repository      = var.repo_name
+  secret_name     = "AZURE_BACKEND_CONTAINER_NAME"
+  plaintext_value = var.backend_container_name
 }
 
 resource "github_actions_secret" "backend_key" {
-  repository       = var.repo_name
-  secret_name      = "AZURE_BACKEND_KEY"
-  plaintext_value  = "${var.repo_name}state.tfstate"
+  repository      = var.repo_name
+  secret_name     = "AZURE_BACKEND_KEY"
+  plaintext_value = "${var.repo_name}state.tfstate"
 }
 
 resource "github_actions_secret" "backend_resource_group_name" {
-  repository       = var.repo_name
-  secret_name      = "AZURE_BACKEND_RESOURCE_GROUP_NAME"
-  plaintext_value  = var.backend_resource_group_name
+  repository      = var.repo_name
+  secret_name     = "AZURE_BACKEND_RESOURCE_GROUP_NAME"
+  plaintext_value = var.backend_resource_group_name
 }
 
 resource "github_actions_secret" "backend_storage_account_name" {
-  repository       = var.repo_name
-  secret_name      = "AZURE_BACKEND_STORAGE_ACCOUNT_NAME"
-  plaintext_value  = var.backend_storage_account_name
+  repository      = var.repo_name
+  secret_name     = "AZURE_BACKEND_STORAGE_ACCOUNT_NAME"
+  plaintext_value = var.backend_storage_account_name
 }
