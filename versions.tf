c07e258e3c24eef4f2e6731218e4dc5a83916fec
Per Næss <perness3009@gmail.com>
2022-07-15 21:20:58 +0200

    feat: bump versions and add github token

--- versions.tf
+++ versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.7.0"
+      version = "=3.14.0"
     }
     github = {
       source  = "integrations/github"
-      version = "~> 4.0"
+      version = "4.26.1"
     }
   }
 }
@@ -18,4 +18,6 @@ provider "azurerm" {
   features {}
 }
 
-provider "github" {}
+provider "github" {
+  token = GITHUB_TOKEN
+}
